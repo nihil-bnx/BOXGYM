a1c82c58f7a3c04ff409b723323c5e1743befbab
nihil-bnx <itzxplyx@gmail.com>
2026-06-19 00:07:06 -0400

    cambios :v

--- src/DATOS/Cliente.java
+++ src/DATOS/Cliente.java
@@ -1,9 +1,9 @@
 package DATOS;
 public class Cliente {
-    String nombre;
-    String apellidos;
-    String DNI;
-    String telefono;
+    private String nombre;
+    private String apellidos;
+    private String DNI;
+    private String telefono;
 
     public Cliente() {
     }
@@ -13,6 +13,38 @@ public class Cliente {
         this.apellidos = apellidos;
         this.DNI = DNI;
         this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
     }
     
     
