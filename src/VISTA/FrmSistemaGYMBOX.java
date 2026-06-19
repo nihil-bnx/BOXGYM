d10cde39c10b45ebe00e04ee94b107702d176bff
nihil-bnx <itzxplyx@gmail.com>
2026-06-19 00:30:42 -0400

    cambios :V

--- src/VISTA/FrmSistemaGYMBOX.java
+++ src/VISTA/FrmSistemaGYMBOX.java
@@ -8,12 +8,12 @@ package Vista;
  *
  * @author aleja
  */
-public class FrmSistemaGB extends javax.swing.JFrame {
+public class FrmSistemaGYMBOX extends javax.swing.JFrame {
 
     /**
      * Creates new form FrmSistemaGB
      */
-    public FrmSistemaGB() {
+    public FrmSistemaGYMBOX() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class FrmSistemaGB extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmSistemaGB.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmSistemaGYMBOX.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmSistemaGB.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmSistemaGYMBOX.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmSistemaGB.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmSistemaGYMBOX.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmSistemaGB.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmSistemaGYMBOX.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmSistemaGB().setVisible(true);
+                new FrmSistemaGYMBOX().setVisible(true);
             }
         });
     }
